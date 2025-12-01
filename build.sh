e45b1cde7148823f2c8e06e6aec20714f18dcb97
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-01 21:53:55 +0530

    Fix: Added collectstatic to build.sh for static file delivery

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py migrate --no-input
+
+# CRITICAL: Collect all static files (CSS, JS, images)
 python manage.py collectstatic --no-input
-python manage.py migrate
