cb21b54617ed325a3045556f70d9b860c46649b8
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-02 18:29:31 +0530

    Fix gemini

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,6 @@
-worker_class = 'uvicorn.workers.UvicornWorker'
+# gunicorn.conf.py (UPDATED)
+
+# worker_class = 'uvicorn.workers.UvicornWorker' # REMOVE OR COMMENT OUT THIS LINE
 workers = 1
 bind = '0.0.0.0:8000'
 timeout = 120
